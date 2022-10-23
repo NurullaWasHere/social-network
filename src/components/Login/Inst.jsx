b35c06e10a61f74070628c687e1294e4e0c7aa1b
Nurulla05 <nurullaamannula05@gmail.com>
2022-10-23 18:03:04 +0600

    inst

--- src/components/Login/Inst.jsx
+++ src/components/Login/Inst.jsx
@@ -1,43 +1,45 @@
-import React from 'react'
-import s from './Inst.module.scss'
-import { Link } from '@mui/icons-material'
-import { TextField, Button } from '@mui/material'
+import React from "react";
+import s from "./Inst.module.scss";
+import { Link } from "@mui/icons-material";
+import { TextField, Button } from "@mui/material";
 
- const Inst = () => {
+const Inst = () => {
   return (
-<div className={s.mainLogin}>
+    <div className={s.mainLogin}>
       <div className={s.mainBlock}>
         <div className={s.loginHeader}>
-          <img width={200} src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png" alt="" />
+          <img
+            width={200}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
+            alt=""
+          />
         </div>
         <div className={s.inputField}>
-
-            <TextField
-              className={s.textField}
-              label="username"
-              placeholder="Phone number, username or email"
-              width={140}
-            />
-            <TextField
-              className={s.textField}
-              placeholder="Введите пароль..."
-              type="password"
-              label="password"
-              width={140}
-            />
-            <Button
-
-            >
-              {" "}
-              Войти
-            </Button>
-
+          <TextField
+            className={s.textField}
+            label="username"
+            placeholder="Phone number, username or email"
+            width={140}
+          />
+          <TextField
+            className={s.textField}
+            placeholder="Введите пароль..."
+            type="password"
+            label="password"
+            width={140}
+          />
+          <Button> Войти</Button>
+          <hr />
+          <div className={s.hr}>
+            <p>или</p>
+          </div>
+          <div className={s.facebook}>Войти через Facebook</div>
+          <div className={s.forgot}>Забыли пароль?</div>
         </div>
-
       </div>
-    </div>
-  )
-}
 
+    </div>
+  );
+};
 
 export default Inst;
